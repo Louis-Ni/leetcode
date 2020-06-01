7f4319e0575344bbad1695cc39a3fbf9c706b5e5
louis9507 <lous.ni@ztore.com>
2020-06-01 22:20:08 +0800

    完成leetcode 35

--- src/question_35/main.go
+++ src/question_35/main.go
@@ -1,37 +1,52 @@
 package main
 
-func main(){
+import "fmt"
 
-}
-func searchInsert(nums []int, target int) int {
-  pos := binarySearch(nums, target)  
-  if pos != -1{
-    return pos
-  }
-  slow:=0  
-  for i:=1; i< len(nums)-1; i++{
-      if target >= nums[slow] && target  <= nums[i]{
-          return i+1
-      }else if target <= nums[slow]{
-          return slow
-      }
-      slow ++
-  }
-  return len(nums)
+func main() {
+	nums := []int {1,3}
+	ans := searchInsert(nums, 2)
+	fmt.Println(ans)
+}func searchInsert(nums []int, target int) int {
+	if len(nums) == 1 {
+		if target == nums[0] {
+			return 0
+		} else if target <= nums[0] {
+			return 0
+		} else {
+			return 1
+		}
+	}
+	pos := binarySearch(nums, target)
+	if pos != -1 {
+		return pos
+	} else {
+		slow := 0
+		fast := 1
+		for i := 0; i < len(nums)-1; i++ {
+			if nums[slow] >= target {
+				return slow
+			}
+			if target >= nums[slow] && target <= nums[fast] {
+				return i+1
+			}
+			slow++
+			fast ++
+		}
+	}
+	return len(nums)
 }
 func binarySearch(nums []int, target int) int {
-  left := 0
-  right := len(nums) -1
-
-  for left <= right{
-    mid := (left+right) / 2
-    if nums[mid] == target{
-      return mid
-    }else if nums[mid] < target{
-      left = mid + 1
-    }else if nums[mid] > target{
-      right = mid - 1
-    }
-  }
-  return -1
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	return -1
 }
