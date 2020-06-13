6c0830da861f402291e384cb128d7ca6dd8ea78c
louis9507 <lous.ni@ztore.com>
2020-06-13 23:44:22 +0800

    更新 题目73做法。

--- src/question_73/main.go
+++ src/question_73/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	nums := [][]int{{0,1,2,0},{3,4,5,2},{1,3,1,5}}
 	setZeroes(nums)
@@ -9,25 +7,51 @@ func main() {
 func setZeroes(matrix [][]int)  {
 	colLength := len(matrix)
 	rowLength := len(matrix[0])
-	res := make([][]int, colLength)
-	for a:=range res{
-		res[a] = append([]int{}, matrix[a]...)
+	isCol := false
+	isRow := false
+	// 记录第一列为0的情况
+	for c:= 0; c<colLength; c++{
+		if matrix[c][0] == 0{
+			isCol = true
+			break
+		}
 	}
 
-	for i:=0; i<colLength; i++{
-		for j:=0; j<rowLength; j++{
+	//记录第一行有0的情况
+	for r:=0; r<rowLength;r++{
+		if matrix[0][r] == 0{
+			isRow = true
+			break
+		}
+	}
+	// 从第1行第1列开始查。如果某个数字是0 就把他当前列和行的第0个设置为0
+	for i:=1; i<colLength; i++{
+		for j:=1; j<rowLength; j++{
 			if matrix[i][j] == 0{
-				fmt.Printf("i:%d,j:%d,num:%d\n",i,j,matrix[i][j])
-				for r:=0; r<rowLength; r++{
-					res[i][r] = 0
-				}
-				for c:=0; c< colLength; c++{
-					res[c][j] = 0
-				}
+				matrix[i][0] = 0
+				matrix[0][j] = 0
 			}
 		}
 	}
-	for m := range matrix{
-		matrix[m] = res[m]
+	// 查找某个被设置成为0的的第一行或第一列。并且把该数值也设置成为0
+	for i:=1; i<colLength; i++{
+		for j:=1; j<rowLength; j++{
+			if matrix[i][0] == 0 || matrix[0][j] == 0{
+				matrix[i][j] = 0
+			}
+		}
+	}
+	//将所有第一列设置为0
+	if isCol == true{
+		for c:= 0; c<colLength; c++{
+			matrix[c][0] = 0
+		}
+	}
+
+	//将第一行设置为0
+	if isRow == true{
+		for r:=0; r<rowLength;r++{
+			 matrix[0][r] = 0
+		}
 	}
 }
