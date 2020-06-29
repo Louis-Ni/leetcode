684c8d97ec22c5627cfefa4b3f9d5ea3eeef9061
louis9507 <lous.ni@ztore.com>
2020-06-29 23:16:15 +0800

    更新题目215 堆的解法

--- src/question_215/main.go
+++ src/question_215/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
 
-func main() {
-	nums := []int{2, 1}
-	ans := findKthLargest(nums, 2)
-	fmt.Println(ans)
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
 }
 
+func (h IntHeap) Less(i, j int) bool{
+	return h[i]<h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+
+func findKthLargest01(nums []int, k int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	sort.Ints(nums)
+	h := &IntHeap{}
+	heap.Init(h)
+	length :=len(nums) - 1
+	for k > 0{
+		heap.Push(h, nums[length])
+		length --
+		k --
+	}
+	return  heap.Pop(h).(int)
+}
 func findKthLargest(nums []int, k int) int {
 	if len(nums) == 1 {
 		return nums[0]
@@ -27,4 +64,9 @@ func findKthLargest(nums []int, k int) int {
 		count--
 	}
 	return ans
+}
+func main() {
+	nums := []int{3,2,1,5,6,4}
+	ans := findKthLargest01(nums, 2)
+	fmt.Println(ans)
 }
